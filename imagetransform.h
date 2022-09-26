ba78d8fbea0abc09ba6e222e035f0fe662a347fe
Samuel van Loggerenberg <zaampie@gmail.com>
2022-09-26 23:23:57 +0200

    Add definition for copying sections of an input image to base transform class

--- imagetransform.h
+++ imagetransform.h
@@ -3,11 +3,18 @@
 #include "leds.h"
 #include "types.h"
 
+struct Offset
+{
+    int x; //x offset from top left
+    int y; //y offset from top left
+};
+
 class ImageTransform
 {
     /* Base class for all objects that transform from an image to ImageStrips */
 public:
     enum TransformType {SimpleTransform, StripTransform, BlockStripTransform, BlockTransform};
+    enum StripPlacement {TopStrip, BottomStrip, LeftStrip, RightStrip};
 
     ImageTransform();
     ~ImageTransform();
@@ -17,5 +24,8 @@ public:
 
     //Virtual function that transform an input image into strips
     virtual ImageStrips transform(QImage inputImage, const StripSizes sizes);
+private:
+    //Copy a specified strip width of an image from a specified side
+    QImage copyStrip(const QImage input, int stripSizePx, StripPlacement placement);
 };
 
