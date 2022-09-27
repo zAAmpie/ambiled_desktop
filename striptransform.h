e7c32ca4e3612d24864413338d7ad05bb30f2ad8
Samuel van Loggerenberg <zaampie@gmail.com>
2022-09-27 13:17:22 +0200

    Implement StripTransform

--- striptransform.h
+++ striptransform.h
@@ -2,17 +2,24 @@
 
 #include "imagetransform.h"
 
+#define DEFAULT_STRIP_WIDTH_PERCENTAGE 0.1
+
 class StripTransform : public ImageTransform
 {
     /* Transform that fast averages into intermediate strips and then smoothly transforms to final strips. Faster than BlocksTripTransform. */
 public:
-    StripTransform();
+    StripTransform(float stripWidthPercentage = DEFAULT_STRIP_WIDTH_PERCENTAGE);
     ~StripTransform();
 
     //Return type
-    TransformType type() const {return TransformType::StripTransform;}
+    TransformType type() const override {return TransformType::StripTransform;}
 
     //Virtual function that transform an input image into strips
-    ImageStrips transform(QImage inputImage, const StripSizes sizes);
+    ImageStrips transform(QImage inputImage, const StripSizes sizes) override;
+private:
+    //Averages an individual strip using block strip
+    QImage averageStrip(const QImage &input, ImageSize size, StripPlacement placement);
+
+    float pStripWidthPercentage;
 };
 
