f2c6f54b2990cc22fd1ed5bb7f71785ea803659e
Samuel van Loggerenberg <zaampie@gmail.com>
2022-09-27 12:48:05 +0200

    Implement ImageTransform

--- imagetransform.cpp
+++ imagetransform.cpp
@@ -1,6 +1,31 @@
 #include "imagetransform.h"
 
+//Constructor
 ImageTransform::ImageTransform()
 {
 
+}
+
+//Destructor
+ImageTransform::~ImageTransform()
+{
+
+}
+
+//Copy a specified strip width of an image from a specified side
+QImage ImageTransform::copyStrip(const QImage input, int stripSizePx, StripPlacement placement)
+{
+    switch (placement)
+    {
+    case TopStrip:
+        return input.copy(0, 0, input.width(), stripSizePx);
+    case BottomStrip:
+        return input.copy(0, input.height() - stripSizePx - 1, input.width(), stripSizePx);
+    case LeftStrip:
+        return input.copy(0, 0, stripSizePx, input.height());
+    case RightStrip:
+        return input.copy(input.width() - stripSizePx - 1, 0, stripSizePx, input.height());
+    default:
+        return QImage();
+    }
 }
