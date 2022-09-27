4a82cf1d7b322c91ca43ba446b0f627a71a092e4
Samuel van Loggerenberg <zaampie@gmail.com>
2022-09-27 12:49:39 +0200

    Implement Screen

--- screen.cpp
+++ screen.cpp
@@ -1,14 +1,10 @@
 #include "screen.h"
 
-Screen::Screen(const CaptureMode type, ScreenSize size) : pType(type), pScreenSize(size)
+Screen::Screen() : pScreenSize(ScreenSize())
 {
     //Default constructor
 }
 
-Screen::Screen(const CaptureMode type) : pType(type), pScreenSize(ScreenSize(0,0))
-{
-    //Convenience constructor to not set a screen size
-}
 
 Screen::~Screen()
 {
